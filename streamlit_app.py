a589e3efabc0c8221f4090025db6df0c8639e134
CalogeroAlfeo <144700685+CalogeroAlfeo@users.noreply.github.com>
2023-09-13 10:30:04 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 #  if not fruit_choice:
 #       streamlit.error("Please select a fruit to get information.")
 #  else:
-#        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
 #        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #        streamlit.dataframe(fruityvice_normalized)
 
@@ -36,7 +36,7 @@ streamlit.dataframe(fruits_to_show)
 #  streamlit.error()
 
 def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
   
@@ -46,10 +46,10 @@ try:
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
-        back_from_function = get_fruityvice_data(this_fruit_choice)
+        back_from_function = get_fruityvice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
-        
-#streamlit.stop()
+
+streamlit.stop()
 
 #import snowflake.connector
 
