94ba84eb4973347be283bc9ce3116bb4fc20a15e
CalogeroAlfeo <144700685+CalogeroAlfeo@users.noreply.github.com>
2023-09-13 10:45:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ def get_fruityvice_data(this_fruit_choice):
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
     
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('Fruityvice Fruit Advice!')
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?')
     if not fruit_choice:
@@ -51,7 +51,7 @@ try:
         
 #import snowflake.connector
 
-streamlit.header("The fruit load list contains:")
+streamlit.header('The fruit load list contains:')
 #Snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
